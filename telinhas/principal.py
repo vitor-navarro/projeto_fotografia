0d358a1efbbe8c7a3beb1f1c99888003637ef1de
vitor navarro <vitorsantosnavarro@gmail.com>
2022-11-02 14:58:15 -0300

    organizado cadastro de trabalhos

--- telinhas/principal.py
+++ telinhas/principal.py
@@ -216,181 +216,99 @@ class Funcs():
     def novo_cadastro_tabalho(self):
         janela = Toplevel()
 
-        janela.title("Cadastro Trabalhos")
-
-        lb_style = ("monospace", 12)
-        entry_style = ("monospace", 14)
-        paddingx = 10
-        paddingy = 7
+        janela.title("Cadastro Trabalho")
 
         self.configurar_janela_auxiliar(janela)
 
         separador1 = ttk.Separator(janela, orient="horizontal")
-        separador1.pack(fill="x", padx=paddingx, pady=paddingy)
+        separador1.pack(fill="x", padx=self.paddingx, pady=self.paddingy)
 
-        lb_codigo = Label(separador1, text="Código", font=lb_style)
-        lb_codigo.grid(column=0, row=0, sticky="NW", padx=paddingx)
-        entry_codigo = Entry(separador1, font=entry_style, width=8, state=DISABLED)
-        entry_codigo.grid(column=0, row=1, padx=paddingx, sticky="NW")
+        lb_codigo = Label(separador1, text="Código", font=self.lb_style)
+        lb_codigo.grid(column=0, row=0, sticky="NW", padx=self.paddingx)
+        entry_codigo = Entry(separador1, font=self.entry_style, width=8, state=DISABLED)
+        entry_codigo.grid(column=0, row=1, padx=self.paddingx, sticky="NW")
 
-        lb_cadastro = Label(separador1, text="Cadastro", font=lb_style)
-        lb_cadastro.grid(column=1, row=0, sticky="W", padx=paddingx)
-        entry_cadastro = Entry(separador1, font=entry_style, width=10)
-        entry_cadastro.grid(column=1, row=1, padx=paddingx)
+        lb_cadastro = Label(separador1, text="Cadastro", font=self.lb_style)
+        lb_cadastro.grid(column=1, row=0, sticky="W", padx=self.paddingx)
+        entry_cadastro = Entry(separador1, font=self.entry_style, width=10)
+        entry_cadastro.grid(column=1, row=1, padx=self.paddingx)
 
-        status_possiveis = ["Ativo", "Inativo"]
-        tipos_possiveis_pessoas = ["Física", "Jurídica"]
+        lb_data_sessao = Label(separador1, text="Data da Sessão", font=self.lb_style)
+        lb_data_sessao.grid(column=2, row=0, sticky="W", padx=self.paddingx)
+        entry_data_sessao = Entry(separador1, font=self.entry_style, width=10)
+        entry_data_sessao.grid(column=2, row=1, padx=self.paddingx)
 
-        # mudar para opções
-        lb_status = Label(separador1, text="Status", font=lb_style)
-        lb_status.grid(column=2, row=0, sticky="W", padx=paddingx)
-        cb_status = ttk.Combobox(separador1, font=entry_style, width=15, values=status_possiveis, state="readonly")
-        cb_status.set("Ativo")
-        cb_status.grid(column=2, row=1, padx=paddingx)
-
-        # mudar para opções
-        lb_tipo = Label(separador1, text="Tipo", font=lb_style)
-        lb_tipo.grid(column=3, row=0, sticky="W", padx=paddingx)
-        cb_tipo = ttk.Combobox(separador1, font=entry_style, width=15, values=tipos_possiveis_pessoas, state="readonly")
-        cb_tipo.set("Física")
-        cb_tipo.grid(column=3, row=1, padx=paddingx)
+        lb_horario_sessao = Label(separador1, text="Horário da Sessão", font=self.lb_style)
+        lb_horario_sessao.grid(column=3, row=0, sticky="W", padx=self.paddingx)
+        entry_horario_sessao = Entry(separador1, font=self.entry_style, width=10)
+        entry_horario_sessao.grid(column=3, row=1, padx=self.paddingx)
 
         separador2 = ttk.Separator(janela, orient="horizontal")
-        separador2.pack(fill="x", padx=paddingx, pady=paddingy)
+        separador2.pack(fill="x", padx=self.paddingx, pady=self.paddingy)
 
-        # condicao para mudar quando for tipo juridica
-        lb_cpf_cnpj = Label(separador2, text="CPF", font=lb_style)
-        lb_cpf_cnpj.grid(column=0, row=0, sticky="W", padx=paddingx)
-        entry_cpf_cnpj = Entry(separador2, font=entry_style, width=20)
-        entry_cpf_cnpj.grid(column=0, row=1, padx=paddingx)
-
-        # condicao para mudar quando for tipo juridica
-        lb_rg_inscricao = Label(separador2, text="RG", font=lb_style)
-        lb_rg_inscricao.grid(column=1, row=0, sticky="W", padx=paddingx)
-        entry_rg_inscricao = Entry(separador2, font=entry_style, width=20)
-        entry_rg_inscricao.grid(column=1, row=1, padx=paddingx)
-
-        lb_nascimento = Label(separador2, text="Nascimento", font=lb_style)
-        lb_nascimento.grid(column=2, row=0, sticky="W", padx=paddingx)
-        entry_nascimento = Entry(separador2, font=entry_style, width=10)
-        entry_nascimento.grid(column=2, row=1, padx=paddingx)
+        lb_nome = Label(separador2, text="Nome", font=self.lb_style)
+        lb_nome.grid(column=0, row=0, sticky="W", padx=self.paddingx)
+        entry_nome = Entry(separador2, font=self.entry_style, width=89)
+        entry_nome.grid(column=0, row=1, padx=self.paddingx)
 
         separador3 = ttk.Separator(janela, orient="horizontal")
-        separador3.pack(fill="x", padx=paddingx, pady=paddingy)
+        separador3.pack(fill="x", padx=self.paddingx, pady=self.paddingy)
 
-        lb_nome = Label(separador3, text="Nome*", font=lb_style)
-        lb_nome.grid(column=0, row=0, sticky="W", padx=paddingx)
-        entry_nome = Entry(separador3, font=entry_style, width=89)
-        entry_nome.grid(column=0, row=1, padx=paddingx)
+        tipos_possiveis = ["Gestante", "Criança"]
 
-        lb_apelido = Label(separador3, text="Apelido", font=lb_style)
-        lb_apelido.grid(column=0, row=2, sticky="W", padx=paddingx)
-        entry_apelido = Entry(separador3, font=entry_style, width=89)
-        entry_apelido.grid(column=0, row=3, padx=paddingx)
+        # mudar para opções
+        lb_tipo_sessao = Label(separador3, text="tipo da Sessão", font=self.lb_style)
+        lb_tipo_sessao.grid(column=0, row=0, sticky="W", padx=self.paddingx)
+        cb_tipo_sessao = Entry(separador3, font=self.entry_style, width=20)
+        #cb_tipo_sessao = ttk.Combobox(separador1, font=self.entry_style, width=15, values=tipos_possiveis, state="readonly")
+        #cb_tipo_sessao.set("Ativo")
+        cb_tipo_sessao.grid(column=0, row=1, padx=self.paddingx,sticky="W")
+
+        planos_possiveis = ["Gestante", "Criança"]
+
+        lb_plano = Label(separador3, text="Plano", font=self.lb_style)
+        lb_plano.grid(column=1, row=0, sticky="W", padx=self.paddingx)
+        cb_plano = Entry(separador3, font=self.entry_style, width=20)
+        #cb_plano = ttk.Combobox(separador1, font=self.entry_style, width=15, values=planos_possiveis, state="readonly")
+        #cb_plano.set("Ativo")
+        cb_plano.grid(column=1, row=1, padx=self.paddingx,sticky="W")
 
         separador4 = ttk.Separator(janela, orient="horizontal")
-        separador4.pack(fill="x", padx=paddingx, pady=paddingy)
+        separador4.pack(fill="x", padx=self.paddingx, pady=self.paddingy)
 
-        lb_endereco = Label(separador4, text="Endereço", font=lb_style)
-        lb_endereco.grid(column=0, row=0, sticky="W", padx=paddingx)
-        entry_endereco = Entry(separador4, font=entry_style, width=82)
-        entry_endereco.grid(column=0, row=1, padx=paddingx)
+        lb_condicao_pagamento1 = Label(separador4, text="Condicao Pagamento", font=self.lb_style)
+        lb_condicao_pagamento1.grid(column=0, row=0, sticky="W", padx=self.paddingx, columnspan=4)
 
-        lb_numero = Label(separador4, text="Numero", font=lb_style)
-        lb_numero.grid(column=1, row=0, sticky="W", padx=paddingx)
-        entry_numero = Entry(separador4, font=entry_style, width=5)
-        entry_numero.grid(column=1, row=1, padx=paddingx)
+        lb_condicao_pagamento_numero1 = Label(separador4, text="1º", font=self.lb_style)
+        lb_condicao_pagamento_numero1.grid(column=0, row=1, padx=self.paddingx,pady=self.paddingy)
+        entry_condicao_pagamento1 = Entry(separador4, font=self.entry_style, width=30)
+        entry_condicao_pagamento1.grid(column=1, row=1, padx=self.paddingx,pady=self.paddingy)
+
+        lb_condicao_pagamento_numero2 = Label(separador4, text="2º", font=self.lb_style)
+        lb_condicao_pagamento_numero2.grid(column=0, row=2, padx=self.paddingx,pady=self.paddingy)
+        entry_condicao_pagamento2 = Entry(separador4, font=self.entry_style, width=30)
+        entry_condicao_pagamento2.grid(column=1, row=2, padx=self.paddingx,pady=self.paddingy)
+
+        lb_condicao_pagamento_numero3 = Label(separador4, text="3º", font=self.lb_style)
+        lb_condicao_pagamento_numero3.grid(column=0, row=3, padx=self.paddingx,pady=self.paddingy)
+        entry_condicao_pagamento3 = Entry(separador4, font=self.entry_style, width=30)
+        entry_condicao_pagamento3.grid(column=1, row=3, padx=self.paddingx,pady= self.paddingy)
 
         separador5 = ttk.Separator(janela, orient="horizontal")
-        separador5.pack(fill="x", padx=paddingx, pady=paddingy)
+        separador5.pack(fill="x", padx=self.paddingx, pady=self.paddingy)
 
-        lb_complemento = Label(separador5, text="Complemento", font=lb_style)
-        lb_complemento.grid(column=0, row=2, sticky="W", padx=paddingx)
-        entry_complemento = Entry(separador5, font=entry_style, width=44)
-        entry_complemento.grid(column=0, row=3, padx=paddingx)
-
-        lb_bairro = Label(separador5, text="Bairro", font=lb_style)
-        lb_bairro.grid(column=1, row=2, sticky="W", padx=paddingx)
-        entry_bairro = Entry(separador5, font=entry_style, width=43)
-        entry_bairro.grid(column=1, row=3, padx=paddingx)
-
-        separador6 = ttk.Separator(janela, orient="horizontal")
-        separador6.pack(fill="x", padx=paddingx, pady=paddingy)
-        '''
-        lb_cidade = Label(separador6, text="Cidade", font=lb_style, state=DISABLED)
-        lb_cidade.grid(column=0, row=4, sticky="W", padx=paddingx)
-        entry_cidade = Entry(separador6, font=entry_style,width=5)
-        entry_cidade.grid(column=0, row=5, padx=paddingx)
-        '''
-
-        lb_nome_cidade = Label(separador6, text="Cidade", font=lb_style)
-        lb_nome_cidade.grid(column=1, row=4, sticky="W", padx=paddingx)
-        entry_nome_cidade = Entry(separador6, font=entry_style, width=40)
-        entry_nome_cidade.grid(column=1, row=5, padx=paddingx)
-
-        lb_uf = Label(separador6, text="UF", font=lb_style)
-        lb_uf.grid(column=2, row=4, sticky="W", padx=paddingx)
-        entry_uf = Entry(separador6, font=entry_style, width=3)
-        entry_uf.grid(column=2, row=5, padx=paddingx)
-
-        lb_cep = Label(separador6, text="CEP", font=lb_style)
-        lb_cep.grid(column=3, row=4, sticky="W", padx=paddingx)
-        entry_cep = Entry(separador6, font=entry_style, width=10)
-        entry_cep.grid(column=3, row=5, padx=paddingx)
-
-        separador7 = ttk.Separator(janela, orient="horizontal")
-        separador7.pack(fill="x", padx=paddingx, pady=paddingy)
-
-        width_fone = 17
-        width_operadora = 8
-        lb_fone1 = Label(separador7, text="Fone 1", font=lb_style)
-        lb_fone1.grid(column=0, row=0, sticky="W", padx=paddingx)
-        entry_fone1 = Entry(separador7, font=entry_style, width=width_fone)
-        entry_fone1.grid(column=0, row=1, padx=paddingx)
-
-        lb_operadora1 = Label(separador7, text="Operadora", font=lb_style)
-        lb_operadora1.grid(column=1, row=0, sticky="W", padx=paddingx)
-        entry_operadora1 = Entry(separador7, font=entry_style, width=width_operadora)
-        entry_operadora1.grid(column=1, row=1, padx=paddingx)
-
-        lb_fone2 = Label(separador7, text="Fone 2", font=lb_style)
-        lb_fone2.grid(column=2, row=0, sticky="W", padx=paddingx)
-        entry_fone2 = Entry(separador7, font=entry_style, width=width_fone)
-        entry_fone2.grid(column=2, row=1, padx=paddingx)
-
-        lb_operadora2 = Label(separador7, text="Operadora", font=lb_style)
-        lb_operadora2.grid(column=3, row=0, sticky="W", padx=paddingx)
-        entry_operadora1 = Entry(separador7, font=entry_style, width=width_operadora)
-        entry_operadora1.grid(column=3, row=1, padx=paddingx)
-
-        lb_fone3 = Label(separador7, text="Fone 3", font=lb_style)
-        lb_fone3.grid(column=4, row=0, sticky="W", padx=paddingx)
-        entry_fone3 = Entry(separador7, font=entry_style, width=width_fone)
-        entry_fone3.grid(column=4, row=1, padx=paddingx)
-
-        lb_operadora3 = Label(separador7, text="Operadora", font=lb_style)
-        lb_operadora3.grid(column=5, row=0, sticky="W", padx=paddingx)
-        entry_operadora3 = Entry(separador7, font=entry_style, width=width_operadora)
-        entry_operadora3.grid(column=5, row=1, padx=paddingx)
-
-        separador8 = ttk.Separator(janela, orient="horizontal")
-        separador8.pack(fill="x", padx=paddingx, pady=paddingy)
-
-        lb_email = Label(separador8, text="Email", font=lb_style)
-        lb_email.grid(column=0, row=0, sticky="W", padx=paddingx)
-        entry_email = Entry(separador8, font=entry_style, width=89)
-        entry_email.grid(column=0, row=1, padx=paddingx)
-
-        lb_campos_obrigatorios = Label(janela, text="(*) Campos Obrigatórios")
-        lb_campos_obrigatorios.pack(fill="x", side=RIGHT, anchor=NW)
+        lb_observacoes = Label(separador5, text="Observações", font=self.lb_style)
+        lb_observacoes.grid(column=0, row=0, padx=self.paddingx,pady=self.paddingy,sticky="W")
+        textarea_observacoes = Text(separador5, font=self.entry_style, height=6)
+        textarea_observacoes.grid(column=0, row=1, padx=self.paddingx,pady= self.paddingy,sticky="W")
 
         separador9 = ttk.Separator(janela, orient="horizontal")
         separador9.pack(fill="x", pady=10, padx=10)
 
-        grava = Button(separador9, text="GRAVA", font=lb_style)
+        grava = Button(separador9, text="GRAVA", font=self.lb_style)
         grava.grid(column=0, row=0, sticky="WS")
 
-        cancela = Button(separador9, text="CANCELA", font=lb_style)
+        cancela = Button(separador9, text="CANCELA", font=self.lb_style)
         cancela.grid(column=1, row=0, sticky="WS")
 
     def novo_financeiro(self):
@@ -798,25 +716,25 @@ class Aplicacao(Funcs):
 
         lb_observacoes = Label(separador2, text="Observações", font=self.lb_style)
         lb_observacoes.grid(column=0, row=0, sticky="NW", padx=self.paddingx, rowspan=3)
-        entry_observacoes = Text(separador2, font=self.entry_style, width=78,height=3, state=DISABLED)
+        entry_observacoes = Text(separador2, font=self.entry_style, width=78,height=3, state=DISABLED,pady=self.paddingy)
         entry_observacoes.grid(column=1, row=0, padx=self.paddingx,sticky="NW")
 
         separador3 = ttk.Separator(separador2, orient="horizontal")
         separador3.grid(column=1, row = 0,rowspan=3,sticky="E")
 
         lb_valor = Label(separador3, text="Valor", font=self.lb_style)
-        lb_valor.grid(column=0, row=0, sticky="NW", padx=self.paddingx)
-        entry_valor = Entry(separador3, font=self.entry_style, width=20, state=DISABLED)
+        lb_valor.grid(column=0, row=0, sticky="NE", padx=self.paddingx)
+        entry_valor = Entry(separador3, font=self.entry_style, width=10, state=DISABLED)
         entry_valor.grid(column=1, row=0, padx=self.paddingx)
 
         lb_pago = Label(separador3, text="Pago", font=self.lb_style)
-        lb_pago.grid(column=0, row=1, sticky="NW", padx=self.paddingx)
-        entry_pago = Entry(separador3, font=self.entry_style, width=20, state=DISABLED)
+        lb_pago.grid(column=0, row=1, sticky="NE", padx=self.paddingx)
+        entry_pago = Entry(separador3, font=self.entry_style, width=10, state=DISABLED)
         entry_pago.grid(column=1, row=1, padx=self.paddingx)
 
         lb_devendo = Label(separador3, text="Devendo", font=self.lb_style)
-        lb_devendo.grid(column=0, row=2, sticky="NW", padx=self.paddingx)
-        entry_devendo = Entry(separador3, font=self.entry_style, width=20, state=DISABLED)
+        lb_devendo.grid(column=0, row=2, sticky="NE", padx=self.paddingx)
+        entry_devendo = Entry(separador3, font=self.entry_style, width=10, state=DISABLED)
         entry_devendo.grid(column=1, row=2, padx=self.paddingx)
 
 
