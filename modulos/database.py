c31e74375a122f3497191b313bb9b8ca71886174
vitor navarro <vitorsantosnavarro@gmail.com>
2022-11-06 07:42:55 -0300

    database pessoas salvando

--- modulos/database.py
+++ modulos/database.py
@@ -11,7 +11,7 @@ def grava_db_pessoa(entry_codigo_valor,entry_cadastro_valor,cb_status_valor,cb_t
                                        entry_nome_valor,entry_apelido_valor,entry_endereco_valor,entry_complemento_valor,entry_bairro_valor,entry_nome_cidade_valor,entry_uf_valor,
                                        entry_cep_valor,entry_fone1_valor,entry_fone2_valor,entry_fone3_valor,entry_operadora1_valor,entry_operadora2_valor,
                                        entry_operadora3_valor,entry_email_valor,entry_numero):
-    codigo_valor= entry_codigo_valor()
+
     cadastro_valor = entry_cadastro_valor()
     status_valor = cb_status_valor()
     tipo_valor = cb_tipo_valor()
@@ -35,14 +35,16 @@ def grava_db_pessoa(entry_codigo_valor,entry_cadastro_valor,cb_status_valor,cb_t
     email_valor = entry_email_valor()
     numero_casa_valor = entry_numero()
 
-    banco,cursor = conecta_db()
+    banco, cursor = conecta_db()
 
-    cursor.execute(f"INSERT INTO pessoas (data_cadastro, nome_razao_social, status, tipo,cpf_cnpj, apelido_fantasia,rg_inscricao_estadual, data_nascimento, endereco, numero_casa, complemento,bairro, cidade, cep, uf,fone1, operador1, fone2, operador2, fone3, operador3, email) VALUES ({cadastro_valor},{nome_valor},{status_valor},{tipo_valor},{cpf_cnpj_valor},{apelido_valor},{rg_inscricao_valor},{nascimento_valor},{endereco_valor},{numero_casa_valor},{complemento_valor},{bairro_valor},{nome_cidade_valor},{cep_valor},{uf_valor},{fone1_valor},{operadora1_valor},{fone2_valor},{operadora2_valor},{fone3_valor},{operadora3_valor},{email_valor})")
 
+    cursor.execute(f"INSERT INTO pessoas (data_cadastro, nome_razao_social, status, tipo,cpf_cnpj, apelido_fantasia,rg_inscricao_estadual, data_nascimento, endereco, numero_casa, complemento,bairro, cidade, cep, uf,fone1, operadora1, fone2, operadora2, fone3, operadora3, email) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(cadastro_valor,nome_valor,status_valor,tipo_valor,cpf_cnpj_valor,apelido_valor,rg_inscricao_valor,nascimento_valor,endereco_valor,numero_casa_valor,complemento_valor,bairro_valor,nome_cidade_valor,cep_valor,uf_valor,fone1_valor,operadora1_valor,fone2_valor,operadora2_valor,fone3_valor,operadora3_valor,email_valor))
+    cursor.execute("select * from pessoas")
+    print(cursor.fetchall())
     banco.commit()
 
     desconecta_db(banco)
-    pass
+
 def desconecta_db(banco):
     banco.close()
 
