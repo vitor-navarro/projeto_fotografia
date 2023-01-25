c557b88aa877dff5a503219bdee0ff697b071ba2
vitor <vitorsantosnavarro@gmail.com>
2023-01-25 20:28:50 -0300

    teste de validador de data

--- testes/teste.py
+++ testes/teste.py
@@ -10,13 +10,38 @@ banco.commit()
 
 banco.close()
 '''
+import re
+import tkinter as tk
+from tkinter import ttk
+from datetime import datetime
 
-from modulos.database import filtro_database_pessoas
 
-filtro = "ATIV"
+def is_valid_date(date_string):
+    print(date_string)
+    ultimo_caractere = date_string[len(date_string) - 1]
 
-print(filtro_database_pessoas(filtro))
+    if re.match(r'^\d{2}/\d{2}/\d{5}$', date_string):
+        try:
+            datetime.strptime(date_string, '%d/%m/%Y')
+            #não retorna nada, se precionar enter vai para o próximo entry
+            return False
+        except ValueError:
+            #retorna no entry de erro ou em popup que a data é inválida
+            return False
 
+    elif ultimo_caractere.isdigit():
+        return True
+    elif ultimo_caractere == "/":
+        return True
+    '''
+
+'''
+
+
+root = tk.Tk()
+date_entry = ttk.Entry(root, width=10, font=('Arial', 16),validate="key", validatecommand=(root.register(is_valid_date), '%P'))
+date_entry.pack()
+root.mainloop()
 
 
 
